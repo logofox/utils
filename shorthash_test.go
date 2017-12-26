10fa091dc87f7a5b095eea52de10577e60b24ec0
Edward <logo_fox@163.com>
2017-12-26 15:57:24 +0800

    Create shorthash_test.go

---
+++ shorthash_test.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Md5(t *testing.T) {
+	data := "logo_fox"
+	fmt.Println("Test_Md5:", Md5(data, false))
+	fmt.Println("Test_Md5 short:", Md5(data, true))
+}
+
+func Test_Bitwise(t *testing.T) {
+	fmt.Println("Test_Bitwise [foobar@example.com]:", Bitwise("foobar@example.com"))
+	fmt.Println("Test_Bitwise:", Bitwise("12"))
+
+	fmt.Println("Test_Bitwise:", Bitwise("z"))
+	fmt.Println("Test_Bitwise:", Bitwise("zf"))
+
+	fmt.Println("Test_Bitwise:", Bitwise("logo"))
+	fmt.Println("Test_Bitwise:", Bitwise("logof"))
+	fmt.Println("Test_Bitwise:", Bitwise("logofox"))
+
+	fmt.Println("Test_Bitwise:", Bitwise("some string"))
+}
+
+func Test_BinaryTransfer(t *testing.T) {
+	fmt.Println("Test_GetBooId [-1007577910]:", BinaryTransfer(-1007577910, 61))
+	fmt.Println("Test_GetBooId:", BinaryTransfer(1097329740, 61))
+}
+
+func Test_ShortHash(t *testing.T) {
+	fmt.Println("Test_ShortHash [logofox]:", ShortHash("logofox"))
+	fmt.Println("Test_ShortHash [foobar@example.com]:", ShortHash("foobar@example.com"))
+	fmt.Println("Test_ShortHash [my name is really big big and big...]:", ShortHash("my name is really big big and big..."))
+	fmt.Println("Test_ShortHash [万里长城永不倒。。。]:", ShortHash("万里长城永不倒。。。"))
+	fmt.Println("Test_ShortHash [和平]:", ShortHash("和平"))
+}
